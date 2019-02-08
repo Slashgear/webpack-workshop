f72cb144a3d32c92bc79e1b139bd4f10ca137cc6
Antoine Caron <antoine.caron@zenika.com>
2019-02-08 14:12:08 +0100

    doc add example on style

---
+++ packages/intermediate/style/src/main.js
@@ -0,0 +1,11 @@
+import "bulma/css/bulma.css";
+import PokemonComponent from "./pokemon.component";
+import { getPokemons } from "./pokemon.service";
+
+const pokemonList = document.querySelector("#pokemons");
+
+getPokemons().then(response => {
+  response.results.map(({ name }, index) => {
+    pokemonList.appendChild(PokemonComponent(name, index + 1));
+  });
+});
