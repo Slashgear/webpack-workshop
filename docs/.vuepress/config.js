c909013408f403cb796526f9b7e3806eff7f3dfc
Antoine CARON <Slashgear@users.noreply.github.com>
2019-03-21 14:30:14 +0100

    feat(i18n): add french translation (#33)
    * feat(i18n): add french translation
    
    * add basics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  title: "Webpack Workshop",
-  description: "Webpack discovering and mastering workshops",
   base: "/webpack-workshop/",
   plugins: {
     "@vuepress/pwa": {
@@ -11,53 +9,91 @@ module.exports = {
       }
     }
   },
-  themeConfig: {
-    lastUpdated: "Last Updated",
-    repo: "Slashgear/webpack-workshop",
-    repoLabel: "Contribute!",
-    docsRepo: "Slashgear/webpack-workshop",
-    docsDir: "docs",
-    editLinks: true,
-    editLinkText: "Help us improve this page!",
-    algolia: {
-      apiKey: "5f0c4bd6212a8fc141c63283636d5228",
-      indexName: "webpack_workshop"
+  locales: {
+    "/": {
+      lang: "en-US", // this will be set as the lang attribute on <html>
+      title: "Webpack Workshop",
+      description: "Webpack discovering and mastering workshops"
     },
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Workshops", link: "/workshops/" }
-    ],
-    sidebar: [
-      "/workshops/",
-      {
-        title: "Novice",
-        collapsable: false,
-        children: [
-          "/workshops/novice/basics",
-          "/workshops/novice/static-assets",
-          "/workshops/novice/code-assets",
-          "/workshops/novice/outputs",
-          "/workshops/novice/novice-koans"
+    "/fr/": {
+      lang: "fr-FR",
+      title: "Ateliers Webpack",
+      description: "Ateliers pour découvrir et maîtriser Webpack"
+    }
+  },
+  themeConfig: {
+    locales: {
+      "/": {
+        selectText: "Languages",
+        label: "English",
+        editLinkText: "Edit this page on GitHub",
+        lastUpdated: "Last Updated",
+        repo: "Slashgear/webpack-workshop",
+        repoLabel: "Contribute!",
+        docsRepo: "Slashgear/webpack-workshop",
+        docsDir: "docs",
+        editLinks: true,
+        algolia: {
+          apiKey: "5f0c4bd6212a8fc141c63283636d5228",
+          indexName: "webpack_workshop"
+        },
+        nav: [
+          { text: "Home", link: "/" },
+          { text: "Workshops", link: "/workshops/" }
+        ],
+        sidebar: [
+          "/workshops/",
+          {
+            title: "Novice",
+            collapsable: false,
+            children: [
+              "/workshops/novice/basics",
+              "/workshops/novice/static-assets",
+              "/workshops/novice/code-assets",
+              "/workshops/novice/outputs",
+              "/workshops/novice/novice-koans"
+            ]
+          },
+          {
+            title: "Intermediate",
+            collapsable: false,
+            children: [
+              "/workshops/intermediate/dev",
+              "/workshops/intermediate/babel",
+              "/workshops/intermediate/style",
+              "/workshops/intermediate/reduce-bundle-size",
+              "/workshops/intermediate/modern-build",
+              "/workshops/intermediate/compression",
+              "/workshops/intermediate/intermediate-koans"
+            ]
+          },
+          {
+            title: "Advanced",
+            collapsable: false,
+            children: ["/workshops/advanced/plugins"]
+          }
         ]
       },
-      {
-        title: "Intermediate",
-        collapsable: false,
-        children: [
-          "/workshops/intermediate/dev",
-          "/workshops/intermediate/babel",
-          "/workshops/intermediate/style",
-          "/workshops/intermediate/reduce-bundle-size",
-          "/workshops/intermediate/modern-build",
-          "/workshops/intermediate/compression",
-          "/workshops/intermediate/intermediate-koans"
+      "/fr/": {
+        selectText: "Langues",
+        label: "Français",
+        editLinkText: "Edit cette page sur Github",
+        lastUpdated: "Mis à jour le",
+        repo: "Slashgear/webpack-workshop",
+        repoLabel: "Contribue !",
+        docsRepo: "Slashgear/webpack-workshop",
+        docsDir: "docs",
+        editLinks: true,
+        nav: [{ text: "Ateliers", link: "/fr/workshops/" }],
+        sidebar: [
+          "/fr/workshops/",
+          {
+            title: "Novice",
+            collapsable: false,
+            children: ["/fr/workshops/novice/basics"]
+          }
         ]
-      },
-      {
-        title: "Advanced",
-        collapsable: false,
-        children: ["/workshops/advanced/plugins"]
       }
-    ]
+    }
   }
 };
