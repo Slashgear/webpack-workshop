0962956fbaf238d9fe1ef24ee53e6715d7d85b84
Gory Alexandre <gory.alexandre02@gmail.com>
2019-10-21 15:33:04 +0200

    fix(intermediate/reduce): use double quote

--- packages/intermediate/reduce-bundle-size/webpack.config.js
+++ packages/intermediate/reduce-bundle-size/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js", // The source module of our dependency graph
